498659da43711524bad81d66e9341827ea71b77f
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-11-12 23:45:01 +0500

    JavaScript: Instructions (Statements)

---
+++ JavaScript Instructions (Statements).js
@@ -0,0 +1,10 @@
+/* Выведите на экран друг за другом три имени: Robert, Stannis, Renly. В результате на экране должно отобразиться:
+
+Robert
+Stannis
+Renly
+Для каждого имени используйте свой собственный вызов console.log(). */
+
+console.log('Robert');
+console.log('Stannis');
+console.log('Renly');
