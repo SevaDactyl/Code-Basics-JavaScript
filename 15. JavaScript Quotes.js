5331dac55d83c6363ae190310e25569ba19b59f5
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-12-03 23:01:42 +0500

    JavaScript: Quotes

---
+++ 15. JavaScript Quotes.js
@@ -0,0 +1,8 @@
+/* Напишите программу, которая выведет на экран:
+
+"Khal Drogo's favorite word is "athjahakar""
+Программа должна в точности вывести на экран именно эту фразу. Обратите внимание на кавычки в начале и в конце фразы:
+
+"Khal Drogo's favorite word is "athjahakar"" */
+
+console.log('"Khal Drogo\'s favorite word is "athjahakar""');
