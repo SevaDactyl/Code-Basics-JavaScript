5a50c14d08243be86b85bcfcf2f850a41c0a6fa1
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-12-16 19:24:51 +0500

    JavaScript: Concatenation

---
+++ 17. JavaScript Concatenation.js
@@ -0,0 +1,10 @@
+/* Выведите на экран
+
+Winter came for the House of Frey.
+используя конкатенацию слов. */
+
+/* eslint no-useless-concat: 0 */
+
+// BEGIN
+console.log('Winter ' + 'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.');
+// END
