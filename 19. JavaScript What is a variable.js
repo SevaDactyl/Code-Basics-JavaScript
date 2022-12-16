41bd408a7070a8ebdf07903a360a0888b76f293e
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-12-16 19:34:20 +0500

    JavaScript: What is a variable

---
+++ 19. JavaScript What is a variable.js
@@ -0,0 +1,8 @@
+/* Создайте переменную с именем motto и содержимым What Is Dead May Never Die!. Распечатайте содержимое переменной. */
+
+/* eslint prefer-const: 0 */
+
+// BEGIN
+let motto = 'What Is Dead May Never Die!';
+console.log(motto);
+// END
