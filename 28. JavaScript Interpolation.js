cceb87a9dc4c5face805abbc9185f7e72de8d643
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-01-03 23:17:03 +0500

    JavaScript: Interpolation

---
+++ 28. JavaScript Interpolation.js
@@ -0,0 +1,11 @@
+/* 
+Выведите на экран строку Do you want to eat, <name>?, где вместо <name> должна использоваться константа stark. Вывод должен получиться таким:
+
+Do you want to eat, Arya?
+*/
+
+const stark = 'Arya';
+
+// BEGIN
+console.log(`Do you want to eat, ${stark}?`);
+// END
