10a7a59ddc091783384670f32e583cf0a9bd1c42
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-03-03 23:07:15 +0500

    JavaScript: JavaScript Mathematical Functions

---
+++ 35. JavaScript JavaScript Mathematical Functions.js
@@ -0,0 +1,13 @@
+/* 
+Выведите на экран абсолютное значение числа, находящегося в константе soldiersCount, используя функцию Math.abs(). Эта функция возвращает абсолютное значение переданного числа. Абсолютное значения числа — это само это число без знака перед ним.
+
+Math.abs(-3); // 3
+Math.abs(3); // 3
+Math.abs(0); // 0
+*/
+
+const soldiersCount = -2309;
+
+// BEGIN (write your solution here)
+console.log(Math.abs(soldiersCount));
+// END
