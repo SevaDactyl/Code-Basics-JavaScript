5891559706dbb2a1b871f11e73fd1d109b4106c2
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-03-24 23:01:03 +0500

    JavaScript: Standard Library

---
+++ 41. JavaScript Standard Library.js
@@ -0,0 +1,12 @@
+/*
+Оператор typeof позволяет определить тип передаваемого операнда. Название типа возвращается в виде строки. Например, вызов typeof 'go go go' вернёт строку 'string' (number — число).
+
+console.log(typeof 3); // => 'number'
+Выведите на экран тип значения константы motto.
+*/
+
+const motto = 'Family, Duty, Honor';
+
+// BEGIN (write your solution here)
+console.log(typeof motto);
+// END
