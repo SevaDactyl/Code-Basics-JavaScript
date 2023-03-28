30b3130c1047a3df3dde157861064cc60fe5491f
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-03-28 23:19:28 +0500

    JavaScript: Methods

---
+++ 43. JavaScript Methods.js
@@ -0,0 +1,9 @@
+/*
+Приведите строку text к нижнему регистру и напечатайте её на экран.
+*/
+
+const text = 'a MIND needs Books as a Sword needS a WHETSTONE, if it is to keep its edge.';
+
+// BEGIN (write your solution here)
+console.log(text.toLowerCase());
+// END
