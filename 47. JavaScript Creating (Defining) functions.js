47ecc9fb80067f931ff75b387405100892455634
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-06-14 23:11:42 +0500

    JavaScript: Creating (Defining) functions

---
+++ 47. JavaScript Creating (Defining) functions.js
@@ -0,0 +1,15 @@
+/*
+Реализуйте функцию с именем printMotto(), которая выведет на экран фразу Winter is coming.
+
+printMotto(); // => Winter is coming
+В задачах, в которых нужно реализовать функцию, эту функцию вызывать не нужно. Вызывать функцию будут автоматизированные тесты, которые проверяют её работоспособность. Пример с вызовом выше показан только для того, чтобы вы понимали, как ваша функция будет использоваться.
+*/
+
+// BEGIN (write your solution here)
+const printMotto = () => {
+    console.log('Winter is coming');
+};
+// END
+
+// exports are studied on Hexlet
+export default printMotto;
