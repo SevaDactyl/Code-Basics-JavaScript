f44623b62f5e63f4cd4b4420fc9b553f1b020cb2
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-06-22 23:13:47 +0500

    JavaScript: Predicates

---
+++ 53. JavaScript Predicates.js
@@ -0,0 +1,14 @@
+/*
+Напишите функцию isMister(), которая принимает строку и проверяет, является ли она словом 'Mister'.
+
+Примеры вызова:
+
+isMister('Mister'); // true
+isMister('Miss');   // false
+*/
+
+// BEGIN (write your solution here)
+const isMister = (age) => age === "Mister";
+// END
+
+export default isMister;
