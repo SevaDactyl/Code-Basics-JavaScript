a658f57404c62f2623e14e0a30cfce481507fa4d
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-07-11 23:50:39 +0500

    JavaScript: Return from Loops

---
+++ 71. JavaScript Return from Loops.js
@@ -0,0 +1,27 @@
+/*
+Реализуйте функцию hasChar(), которая проверяет (с учётом регистра), содержит ли строка указанную букву. Функция принимает два параметра:
+
+Строка
+Буква для поиска
+hasChar('Hexlet', 'H'); // true
+hasChar('Hexlet', 'h'); // false
+hasChar('Awesomeness', 'm'); // true
+hasChar('Awesomeness', 'd'); // false
+*/
+
+// BEGIN (write your solution here)
+const hasChar = (str, char) => {
+    let i = 0;
+    while (i < str.length) 
+    {
+        if (str[i] === char) 
+        {
+            return true;
+        }
+        i += 1;
+    }
+    return false;
+};
+// END
+
+export default hasChar;
